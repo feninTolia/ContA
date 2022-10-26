0703d7d2885d9bd7538010d4df2cb8add6fa3397
feninTolia <fenintolja@gmail.com>
2022-10-26 23:04:04 +0300

    added styles for sign up form

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/authThunks';
+import css from 'components/SignInForm/SignInForm.module.css';
 
 const SignInForm = () => {
   const [signInData, setSignInData] = useState({
@@ -21,44 +22,52 @@ const SignInForm = () => {
   };
 
   return (
-    <>
-      <h3>Sign in form</h3>
-      <form>
-        <label>
+    <div className={css.wrapper}>
+      <span className={css.logo}>🧚🏻‍♀️</span>
+      <h3 className={css.title}>Sign in to ContA</h3>
+      <form className={css.form}>
+        <label className={css.label}>
           Name
           <input
             type="text"
             name="name"
             onChange={handleInputsChange}
             value={signInData.name}
+            className={css.input}
           />
         </label>
         <br />
-        <label>
+        <label className={css.label}>
           Email
           <input
             type="email"
             name="email"
             onChange={handleInputsChange}
             value={signInData.email}
+            className={css.input}
           />
         </label>
         <br />
-        <label>
+        <label className={css.label}>
           Password
           <input
             type="password"
             name="password"
             onChange={handleInputsChange}
             value={signInData.password}
+            className={css.input}
           />
         </label>
         <br />
-        <button type="button" onClick={handleRegisterBtnClicK}>
+        <button
+          type="button"
+          onClick={handleRegisterBtnClicK}
+          className={css.button}
+        >
           Sign in
         </button>
       </form>
-    </>
+    </div>
   );
 };
 

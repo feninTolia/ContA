f2cd7f5aec2721d2ccdc2c76a399ae717249bb4d
feninTolia <fenintolja@gmail.com>
2022-11-20 12:45:23 +0200

    fix 20.11

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="goit-react-hw-08-phonebook">
+    <BrowserRouter basename="ContA">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <App />
